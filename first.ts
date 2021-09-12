0e43c6a1811282635e3e81f3eb064dac86762862
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-12 22:06:01 +0600

    ts setup

---
+++ first.ts
@@ -0,0 +1,4 @@
+console.log('hello ts');
+const n = 6;
+const myName = 'Najmul';
+console.log(myName, n);
---
+++ js/first.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hello ts');
+const n = 6;
+const myName = 'Najmul';
+console.log(myName, n);
